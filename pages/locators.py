47ce63636ec046029dee0f97f62a71e614902203
Veronika <veronikagurova@bk.ru>
2021-10-12 23:29:02 +0500

    Move methods for going to the login page to BasePage class

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,5 @@
 from selenium.webdriver.common.by import By
 
-
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
@@ -14,3 +10,7 @@ class ProductPageLocators():
     PRODUCT_PAGE_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
     BASKET_PRICE = (By.CSS_SELECTOR, ".alert-info strong")
     PRODUCT_PRICE =(By.CSS_SELECTOR, ".col-sm-6.product_main .price_color")
+    SUCCESS_MESSAGE =(By.CSS_SELECTOR, ".alert-success:nth-child(1)")
+    
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 import pytest
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -19,3 +20,38 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_match_of_product_names_in_basket_message_and_product_page()
     page.should_be_basket_price_equal_to_added_product_price()
 
+@pytest.mark.xfail(reason="bug won't be fixed")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_basket_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_basket_message()
+
+@pytest.mark.xfail(reason="bug won't be fixed")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    success_message_should_disappear()
+    
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
